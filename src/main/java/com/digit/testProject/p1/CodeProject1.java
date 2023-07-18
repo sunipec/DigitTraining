c2edbf457a35a4edb53ace2751cf4a69281abfbc
Suraj.Singh <Suraj.Singh@LDIGITBGL--L0263.godigit.corp>
2023-07-18 13:10:04 +0530

    new changes

---
+++ src/main/java/com/digit/testProject/p1/CodeProject1.java
@@ -0,0 +1,11 @@
+package com.digit.testProject.p1;
+
+public class CodeProject1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Suraj singh");
+		
+		System.out.println("My favorite player is ms dhoni");
+
+	}
+}
